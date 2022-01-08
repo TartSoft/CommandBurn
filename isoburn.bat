597aa0c4de6b10406fa787fffb8428d0b30fb30b
Le Trieu Duong <duong032589@stu.vinschool.edu.vn>
2022-01-08 21:48:35 +0700

    Update isoburn.bat

--- isoburn.bat
+++ isoburn.bat
@@ -1,1 +1,11 @@
+REM This program use isoburn.exe util to burn ISO to Disc
+@echo off
+color 01
+title ISOBurn
+:isoburnstart
+set /p DiskOutput=Choose what disk you want to burn the ISO file to:
+set /p FullPath=Type the full path to the ISO file. Ex: D:\Your Folder Name\ISO File Name.iso
+isoburn.exe /Q %DiskOutput% "%FullPath%\%ISOName%.iso"
+pause
+goto isoburnstart
 
